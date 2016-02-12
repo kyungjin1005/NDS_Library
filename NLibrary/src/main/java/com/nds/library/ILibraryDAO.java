5b3bf5cd86762c93872405cdf4147ef0a1c77db5
Kyungjin <kyungjin1005@naver.com>
2016-02-12 10:37:05 +0900

    change NLibrary directory

--- NLibrary/src/main/java/com/nds/library/ILibraryDAO.java
+++
@@ -1,9 +0,0 @@
-package com.nds.library;
-
-import java.util.ArrayList;
-import java.util.Map;
-
-public interface ILibraryDAO {
-	public ArrayList<Book> requireBookList(Map<String, Object> map);
-
-}
